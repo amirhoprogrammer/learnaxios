6bde33ca8b91f92744e5dd3c4abb5c863ad80a5d
amihoprogrammer <amirhanegareshi@gmail.com>
2025-08-01 09:45:40 +0330

    fix the css of Home and Postlist

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
+import "../App.css";
 function Home() {
   return (
     <div>
       <h1>axios lessons</h1>
-      <div>
-        <div>
+      <div className="box">
+        <div className="card">
           <p>lesson1</p>
           <a href="/postlist">go to lesson1</a>
+        </div>
+        <div className="card">
+          <p>lesson2</p>
+          <a href="/postlist">go to lesson2</a>
         </div>
       </div>
     </div>
